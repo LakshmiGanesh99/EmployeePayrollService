7c6d78ea5363b6eefe0dd8d6b95c7d1247cc406c
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-20 00:48:18 +0530

    [LakshmiGanesh] add: initial commit

---
+++ Employee-Payroll-Service/src/main/java/com/capgemini/employeePayrollService/Employee_Payroll_Service/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.employeePayrollService.Employee_Payroll_Service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Employee-Payroll-Service/src/test/java/com/capgemini/employeePayrollService/Employee_Payroll_Service/AppTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.employeePayrollService.Employee_Payroll_Service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
